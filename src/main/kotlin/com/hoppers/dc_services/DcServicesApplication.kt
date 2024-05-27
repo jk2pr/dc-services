cca9b765ea2ad2ed1c29af2fa23cc2e0fad270cd
Jitendra <jitendra.prajapati@bolttech.io>
2024-05-27 09:21:51 +0800

    Initial commit

---
+++ src/main/kotlin/com/hoppers/dc_services/DcServicesApplication.kt
@@ -0,0 +1,11 @@
+package com.hoppers.dc_services
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DcServicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DcServicesApplication>(*args)
+}
---
+++ src/test/kotlin/com/hoppers/dc_services/DcServicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hoppers.dc_services
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DcServicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
